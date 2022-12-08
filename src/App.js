75ea85a3253081d8974672b8611cd32118331105
Brandon Knight <knightbrandon1984@gmail.com>
2022-12-08 11:57:50 -0600

    wildcard

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
         <Route path="contact" element={<Contact />} />
         <Route path="portfolio" element={<Portfolio />} />
         <Route path="resume" element={<Resume />} />
+        <Route path='*' element={<p>You made it</p>} />
         </Route>
     </Routes>
     </>
